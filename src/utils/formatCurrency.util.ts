45144a76a90ad540fad6b615dfb98c0a2daf2d72
SandroGreenn <sandro@greenn.com.br>
2025-08-24 12:29:38 -0300

    ajuste format currency

--- src/utils/formatCurrency.util.ts
+++ src/utils/formatCurrency.util.ts
@@ -4,28 +4,23 @@
  * @param value The value to be formatted. Can be a number or a string (e.g., "2000.00").
  * @returns The formatted currency string, e.g., "R$ 2.000,00".
  */
-export default function formatToBRL(value: number | string): string {
+export default function formatToBRL(value: number | string | undefined): string {
   let numericValue: number;
 
-  // Verifica o tipo do valor de entrada
   if (typeof value === 'string') {
-    // Tenta converter a string para um número
     numericValue = parseFloat(value);
   } else if (typeof value === 'number') {
     numericValue = value;
   } else {
-    // Lida com tipos de entrada inválidos
     console.error('Invalid value provided to formatToBRL. Expected a number or a string.');
-    return ''; // Ou retorne um valor padrão, como "R$ 0,00"
+    return '';
   }
 
-  // Verifica se a conversão resultou em um número válido
   if (isNaN(numericValue)) {
     console.error('Failed to convert the provided value to a valid number.');
     return '';
   }
 
-  // Use a API Intl.NumberFormat para formatação
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
