4172adb7a79132ba194c4bd95c2029dae066a7a5
SandroGreenn <sandro@greenn.com.br>
2025-11-17 21:00:42 -0300

    ajuste listagem evaluations models

--- src/components/system/evaluation/tabs/ModelsTab.vue
+++ src/components/system/evaluation/tabs/ModelsTab.vue
@@ -4,11 +4,10 @@ import { useEvaluationStore } from '@/stores/evaluation.store';
 import loadItems from '@/utils/loadItems.util';
 import type Evaluation from '@/types/evaluation/evaluation.type';
 import EvaluationModal from '@/components/system/evaluation/EvaluationModal.vue';
-import { useAccountUserStore } from '@/stores/account-user.store';
 import { useDRDStore } from '@/stores/drd.store';
+import { getInitials } from '@/utils/getInitialsFromName.util';
 
 const evaluationStore = useEvaluationStore();
-const accountUserStore = useAccountUserStore();
 const drdStore = useDRDStore();
 
 const dialog = ref(false);
@@ -81,9 +80,9 @@ onMounted(async () => {
 
     <v-data-table
       :headers="[
-        { title: 'Título', value: 'title', sortable: true },
-        { title: 'Criado por', value: 'created_by_user_id', sortable: true },
-        { title: 'DRD', value: 'drd_id', sortable: true },
+        { title: 'Nome', value: 'name', sortable: true },
+        { title: 'Criado por', value: 'createdBy', sortable: true },
+        { title: 'DRD', value: 'drd', sortable: true },
         { title: 'Ações', value: 'actions', sortable: false, align: 'end' }
       ]"
       :items="evaluationStore.evaluations || []"
@@ -96,6 +95,25 @@ onMounted(async () => {
       mobile-breakpoint="md"
       @update:options="loadEvaluations"
     >
+      <template #item.createdBy="{ item }">
+        <div class="d-flex align-center gap-3 flex-row-reverse flex-md-row">
+          <v-avatar color="primary" size="36" class="mr-2 ml-sm-2 mr-sm-0">
+            <template v-if="item.createdBy?.profile_img_url">
+              <v-img :src="item.createdBy.profile_img_url"></v-img>
+            </template>
+            <template v-else>
+              {{ getInitials(item.createdBy?.name) }}
+            </template>
+          </v-avatar>
+          <div class="text-md-left text-right text-left">
+            <div class="font-weight-medium">{{ item.createdBy?.name }}</div>
+            <div class="text-caption text-medium-emphasis">{{ item.createdBy?.email }}</div>
+          </div>
+        </div>
+      </template>
+      <template #item.drd="{ item }">
+        {{ item.drd?.jobPosition.title }}
+      </template>
       <template v-slot:[`item.actions`]="{ item }">
         <div>
           <v-btn icon @click="openDialog(item)" size="small">
--- src/types/evaluation/evaluation.type.ts
+++ src/types/evaluation/evaluation.type.ts
@@ -1,13 +1,15 @@
+import type DRD from '../drd/drd.type';
 import type Form from '../form/form.type';
-import type User from '../user/user.type';
-import type EvaluationTopic from './evaluation-topic.type';
+import type { UserAvatar } from '../user/user-avatar.type';
 
 export default interface Evaluation {
   uuid: string;
   name: string;
   description: string;
   created_by_user_uuid: string;
+  createdBy: UserAvatar;
   rate: number;
   drd_uuid?: string;
+  drd?: DRD;
   form: Form;
 }
