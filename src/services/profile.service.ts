dbda7bed1ce806075b8182899cee037f3163d630
SandroAS <77994311+SandroAS@users.noreply.github.com>
2025-12-31 19:26:34 -0300

    ajustes api de users

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -6,7 +6,7 @@ import type { ProfileCompany } from '@/types/profile/profile-company.type';
 import type ProfilePassword from '@/types/profile/profile-password.type';
 
 export const updateUserPersonalInformation = async (uuid: string, personalInformation: FormData): Promise<ProfilePersonalInformationResponse> => {
-  const response: AxiosResponse<ProfilePersonalInformationResponse> = await api.put(`/user/personal-information/${uuid}`, personalInformation, {
+  const response: AxiosResponse<ProfilePersonalInformationResponse> = await api.put(`/users/personal-information/${uuid}`, personalInformation, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -22,6 +22,6 @@ export const saveUserCompany = async (company: ProfileCompany, uuid?: string) =>
 };
 
 export const saveUserPassword = async (uuid: string, password: ProfilePassword) => {
-  const response: AxiosResponse<boolean> = await api.put(`/user/password/${uuid}`, password);
+  const response: AxiosResponse<boolean> = await api.put(`/users/password/${uuid}`, password);
   return response.data;
 };
